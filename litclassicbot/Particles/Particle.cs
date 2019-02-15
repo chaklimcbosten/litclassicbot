686f991af4ffea3a237cc30f623a1a74e9a0192c
Михаил <goloto@live.com>
2019-02-15 20:27:50 +0300

    v0.3.3(10) Всё ещё медленно рождаются намётки совершенствования кода.

---
+++ litclassicbot/Particles/Particle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace litclassicbot.Classes
+{
+    public class Particle
+    {
+        private int id;
+        private string body;
+        private string title;
+
+        public int Id { get => id; set => id = value; }
+        public string Body { get => body; set => body = value; }
+        public string Title { get => title; set => title = value; }
+    }
+}
---
+++ litclassicbot/Words/Word.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace litclassicbot.Classes
+{
+    public class Word
+    {
+        private int id;
+        private string word;
+        private string body;
+
+        public int Id { get => id; set => id = value; }
+        public string Word { get => word; set => word = value; }
+        public string Body { get => body; set => body = value; }
+    }
+}
